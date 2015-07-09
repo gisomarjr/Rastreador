ebf2efe4fddb33348562711bcc981f9ec47597b9
Gisomar <gisomarjr1994@gmail.com>
2015-07-08 23:24:44 -0300

    SQL - Convertido para Mysql

--- Rastreador/SQL/ScriptSQL.sql
+++ Rastreador/SQL/ScriptSQL.sql
@@ -1,77 +1,93 @@
 -- Geração de Modelo físico
--- Sql ANSI 2003 - brModelo.
+-- Rastreador - SQL -> MySQL.
 
-
-
-CREATE TABLE Técnico (
-Matrícula Texto(1),
-Disponível Texto(1),
-Id_tecnico Texto(1) PRIMARY KEY,
-Nome_tecnico Texto(1)
-)
+CREATE TABLE Tecnico (
+Matricula text(60),
+Disponivel text(10),
+Nome_tecnico text(600),
+Id_tecnico int(5) unsigned NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (Id_tecnico)
+);
 
 CREATE TABLE Loja (
-Nome_Loja Texto(1),
-Id_loja Texto(1) PRIMARY KEY
-)
+Nome_Loja text(800),
+Id_loja int(5) unsigned NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (Id_loja)
+);
 
 CREATE TABLE OS (
-Status Texto(1),
-Id_OS Texto(1) PRIMARY KEY
-)
+Status text(100),
+Id_OS int(5) unsigned NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (Id_OS)
+);
 
-CREATE TABLE Instalação (
-Id_instalacao Texto(1) PRIMARY KEY,
-Data Texto(1),
-Hora Texto(1),
-Endereço Texto(1),
-Id_cliente Texto(1),
-Id_OS Texto(1),
-Placa Texto(1),
-Id_tecnico Texto(1),
-Id_usuario Texto(1),
+CREATE TABLE Instalacao (
+Id_instalacao int(5) unsigned NOT NULL AUTO_INCREMENT,
+Data text(30),
+Hora text(30),
+Endereço text(900),
+Id_cliente int(5),
+Id_OS int(5),
+Placa text(100),
+Id_tecnico int(5),
+Id_usuario int(5),
+PRIMARY KEY (Id_instalacao)/*,
 FOREIGN KEY(Id_OS) REFERENCES OS (Id_OS),
-FOREIGN KEY(Id_tecnico) REFERENCES Técnico (Id_tecnico)
-)
+FOREIGN KEY(Id_tecnico) REFERENCES Tecnico (Id_tecnico)*/
+);
 
-CREATE TABLE Veículo (
-Modelo Texto(1),
-Placa Texto(1) PRIMARY KEY
-)
+CREATE TABLE Veiculo (
+Id_Veiculo int(5) unsigned NOT NULL AUTO_INCREMENT,
+Modelo text(800),
+Placa text(60),
+PRIMARY KEY (Id_Veiculo)
+);
 
 CREATE TABLE Planos (
-Id Texto(1) PRIMARY KEY,
-Tipo Texto(1),
-Id_instalacao Texto(1),
-FOREIGN KEY(Id_instalacao) REFERENCES Instalação (Id_instalacao)
-)
+Id int(5) unsigned NOT NULL AUTO_INCREMENT,
+Tipo text(800),
+Id_instalacao int(5),
+PRIMARY KEY (Id)/*,
+FOREIGN KEY(Id_instalacao) REFERENCES Instalacao (Id_instalacao)*/
+);
 
-CREATE TABLE Usuário (
-Senha Texto(1),
-Tipo Texto(1),
-Login Texto(1),
-Id_usuario Texto(1) PRIMARY KEY,
-Nome Texto(1),
-Id_loja Texto(1),
-FOREIGN KEY(Id_loja) REFERENCES Loja (Id_loja)
-)
+CREATE TABLE Usuario (
+Senha text(800),
+Tipo text(800),
+Login text(800),
+Id_usuario int(5) unsigned NOT NULL AUTO_INCREMENT,
+Nome text(900),
+Id_loja int(5),
+PRIMARY KEY (Id_usuario)/*,
+FOREIGN KEY(Id_loja) REFERENCES Loja (Id_loja)*/
+);
 
 CREATE TABLE Cliente (
-Endereco Texto(1),
-RG Texto(1),
-Id_cliente Texto(1) PRIMARY KEY,
-Cpf Texto(1),
-Nome_cliente Texto(1)
-)
+Endereco text(900),
+RG text(30),
+Id_cliente int(5) unsigned NOT NULL AUTO_INCREMENT,
+Cpf text(14),
+Nome_cliente text(900),
+PRIMARY KEY (Id_cliente)
+);
 
 CREATE TABLE Analisa (
-Situacao Texto(1),
-Id_analise Texto(1) PRIMARY KEY,
-Id_cliente Texto(1),
-Id_usuario Texto(1),
+Situacao text(900),
+Id_analise int(5) unsigned NOT NULL AUTO_INCREMENT,
+Id_cliente int(5),
+Id_usuario int(5),
+PRIMARY KEY (Id_analise)/*,
 FOREIGN KEY(Id_cliente) REFERENCES Cliente (Id_cliente)/*falha: chave estrangeira*/
-)
+);
+/*
+ALTER TABLE Instalacao ADD CONSTRAINT Id_cliente  FOREIGN KEY (Id_cliente) REFERENCES Cliente ;
+ALTER TABLE Instalacao ADD CONSTRAINT Id_Veiculo  FOREIGN KEY (Id_Veiculo) REFERENCES Veiculo ;
+ALTER TABLE Instalacao ADD CONSTRAINT Id_usuario  FOREIGN KEY (Id_usuario) REFERENCES Usuario ;
 
-ALTER TABLE Instalação ADD FOREIGN KEY(Id_cliente) REFERENCES Cliente (Id_cliente)
-ALTER TABLE Instalação ADD FOREIGN KEY(Placa) REFERENCES Veículo (Placa)
-ALTER TABLE Instalação ADD FOREIGN KEY(Id_usuario) REFERENCES Usuário (Id_usuario)
+*/
+
+
+
+
+
+
