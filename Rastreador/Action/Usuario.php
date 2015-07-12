bf3841b8216527ed56545b278bf6759eb95378e7
Gisomar <gisomarjr1994@gmail.com>
2015-07-12 19:09:41 -0300

    Login usuario e interno - cadatro

--- Rastreador/Action/Usuario.php
+++ Rastreador/Action/Usuario.php
@@ -4,10 +4,21 @@ include '../Controller/Usuario.php';
 ini_set('display_errors', '1');
 
     $acao = $_POST['acao'];
-
+   
     switch ($acao){
         case "cadastrar":
-            ActionUsuario::cadastrar($senha, $tipo, $login, $nome, $idLoja);
+            $senha = $_POST['senha'];
+            $tipo  = $_POST['tipo'];
+            $login = $_POST['login'];
+            $nome  = $_POST['nome'];
+            
+            ActionUsuario::cadastrar($senha, $tipo, $login, $nome);
+            break;
+        case "login":
+            $senha = $_POST['senha'];
+            $tipo  = $_POST['tipo'];
+            $login = $_POST['login'];
+            ActionUsuario::login($senha, $tipo, $login);
             break;
     }
 
@@ -15,10 +26,16 @@ ini_set('display_errors', '1');
  class ActionUsuario
 {
   
-      static function cadastrar($senha,$tipo,$login,$nome,$idLoja)
+     static function cadastrar($senha,$tipo,$login,$nome)
      {
+        
         $ControllerUsuario = new ControllerUsuario();
-        $ControllerUsuario->cadastrar($senha, $tipo, $login, $nome, $idLoja);
+        $ControllerUsuario->cadastrar($senha, $tipo, $login, $nome);
      }
     
+     static function login($senha,$tipo,$login)
+     {
+        $ControllerUsuario = new ControllerUsuario();
+        $ControllerUsuario->login($senha, $tipo, $login);
+     }
 }
--- Rastreador/Controller/Usuario.php
+++ Rastreador/Controller/Usuario.php
@@ -5,10 +5,35 @@ ini_set('display_errors', '1');
 
 class ControllerUsuario
 {
-     function cadastrar($senha,$tipo,$login,$nome,$idLoja)
+     function cadastrar($senha,$tipo,$login,$nome)
      {
         $DAOUsuario = new Usuario();
-        $DAOUsuario->cadastrar($senha, $tipo, $login, $nome, $idLoja);
+        $DAOUsuario->cadastrar($senha, $tipo, $login, $nome);
      }
-    
+
+     function login($senha, $tipo, $login) 
+     {
+        session_start(); 
+        
+        $dados = array();
+        
+        $DAOUsuario = new Usuario();
+        $dados = $DAOUsuario->login($senha, $tipo, $login);
+        
+        if(count($dados) > 0){
+            
+            $_SESSION['id']   = $dados['id'];
+            $_SESSION['nome'] = $dados['nome'];
+            $_SESSION['tipo'] = $dados['tipo'];
+
+             header("Location: /View/Cliente/interno.php");  
+        
+             
+        }else{
+            
+            header("Location: /index.php"); 
+           
+        }
+     }
+
 }
